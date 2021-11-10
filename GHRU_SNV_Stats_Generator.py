1e07b5c68e2533ab3273fec18be3c078da0e19b1
Zoe Anne Dyson <zadyson@gmail.com>
2021-11-10 12:29:37 +0000

    Updated to include only alt calls in counts

--- GHRU_SNV_Stats_Generator.py
+++ GHRU_SNV_Stats_Generator.py
@@ -8,7 +8,7 @@
 # Dependencies:
 #	 SAMtools and bcftools are required.
 #
-# Last modified - November 8th, 2021
+# Last modified - November 10th, 2021
 #
 
 # Import required packages
@@ -16,7 +16,7 @@ from argparse import (ArgumentParser, FileType)
 import os, sys, re, collections, operator
 
 # Version number
-__version__='0.0.1'
+__version__='0.0.2'
 
 # Set up argument parser 
 def parse_args():
@@ -28,6 +28,8 @@ def parse_args():
 		help='File path to bcf(s) to be analysed')
 	parser.add_argument('--chromosome_size', type=int, required=True,
 		help='Size of reference chromsome')
+	parser.add_argument('--output_prefix', type=str, required=True,
+		help='Output file prefix')
 
 	return parser.parse_args()
 
@@ -37,7 +39,7 @@ def main():
 	args = parse_args()
 
 	# Output summary file with header
-	output_snv_summary = open("Summary.csv", "w")
+	output_snv_summary = open(args.output_prefix  + "_snv_qc_summary.tsv", "w")
 	output_snv_summary.write("File \t PASS \t LowQual \t Percent_LowQual \n")
 
 	# Open bcf file
@@ -46,8 +48,8 @@ def main():
 				os.system("bcftools view " + bcf + " | bcftools view > " + bcf + ".vcf")
 
 				# Set counters to zero
-				good_snv_count = 0
-				bad_snv_count = 0
+				pass_snv_count = 0
+				lowqual_snv_count = 0
 
 				# Open created vcf file
 				bcf_file = open(bcf + ".vcf", 'r')
@@ -56,20 +58,21 @@ def main():
 				for line in bcf_file:
 					if not line.startswith('#'):
 						x = line.rstrip().split()
-						if int(x[1]) <= args.chromosome_size:
-							if (x[6]=="PASS"):
-								good_snv_count = good_snv_count + 1
-							if (x[6]=="LowQual"):
-								bad_snv_count = bad_snv_count + 1
-				good_bad_percent = (float(bad_snv_count)/float(good_snv_count))*100
+						if x[4]!=".":
+							if int(x[1]) <= args.chromosome_size:
+								if (x[6]=="PASS"):
+									pass_snv_count = pass_snv_count + 1
+								if (x[6]=="LowQual"):
+									lowqual_snv_count = lowqual_snv_count + 1
+				lowqual_percent = (float(lowqual_snv_count)/float(pass_snv_count))*100
 
 				# Close file for buffering
 				bcf_file.close()
 				# delete temp vcf file
 				os.system("rm " + bcf + ".vcf")
 				# Output summary data
-				output_snv_summary.write(bcf + '\t' + 
-					str(good_snv_count) + '\t' + str(bad_snv_count) + '\t' + str(good_bad_percent) + '\n')
+				output_snv_summary.write(os.path.basename(bcf) + '\t' + 
+					str(pass_snv_count) + '\t' + str(lowqual_snv_count) + '\t' + str(lowqual_percent) + '\n')
 	output_snv_summary.close()
 
 # call main function
