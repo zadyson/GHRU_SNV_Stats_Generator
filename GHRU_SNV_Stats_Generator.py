921577cf3d77c75eae3531711aa4f0f56ac81ccd
Zoe Anne Dyson <zadyson@gmail.com>
2021-11-10 14:37:29 +0000

    Ignores indels & writes temp files to script dir

--- GHRU_SNV_Stats_Generator.py
+++ GHRU_SNV_Stats_Generator.py
@@ -16,7 +16,7 @@ from argparse import (ArgumentParser, FileType)
 import os, sys, re, collections, operator
 
 # Version number
-__version__='0.0.2'
+__version__='0.0.3'
 
 # Set up argument parser 
 def parse_args():
@@ -38,41 +38,57 @@ def parse_args():
 def main():
 	args = parse_args()
 
+	# Get location of script
+	pwd = os.getcwd()
+
 	# Output summary file with header
 	output_snv_summary = open(args.output_prefix  + "_snv_qc_summary.tsv", "w")
 	output_snv_summary.write("File \t PASS \t LowQual \t Percent_LowQual \n")
 
 	# Open bcf file
 	for bcf in args.bcf:
-				# Convert bcf to vcf and make vcf file
-				os.system("bcftools view " + bcf + " | bcftools view > " + bcf + ".vcf")
+				# Convert bcf to vcf and make temp vcf file in script directory
+				os.system("bcftools view " + bcf + " | bcftools view > " + pwd + "/" + os.path.basename(bcf) + ".vcf")
 
-				# Set counters to zero
+				# initiate counter variables to zero
 				pass_snv_count = 0
 				lowqual_snv_count = 0
+				lowqual_percent = 0
 
 				# Open created vcf file
-				bcf_file = open(bcf + ".vcf", 'r')
+				bcf_file = open(pwd + "/" + os.path.basename(bcf) + ".vcf", 'r')
 
-				# Extract good/bad SNV calls & ratio
+				# Extract PASS/LowQual SNV calls & ratio
 				for line in bcf_file:
+					# Ignore header lines
 					if not line.startswith('#'):
-						x = line.rstrip().split()
-						if x[4]!=".":
-							if int(x[1]) <= args.chromosome_size:
-								if (x[6]=="PASS"):
-									pass_snv_count = pass_snv_count + 1
-								if (x[6]=="LowQual"):
-									lowqual_snv_count = lowqual_snv_count + 1
+						# Ignore indels
+						if "INDEL" not in line:
+							x = line.rstrip().split()
+							# Ignore reference alleles
+							if x[4]!=".":
+								# Ingore concatenated sequences (e.g. plasmids)
+								if int(x[1]) <= args.chromosome_size:
+									# Count PASS and LowQual SNVs
+									if (x[6]=="PASS"):
+										pass_snv_count = pass_snv_count + 1
+									if (x[6]=="LowQual"):
+										lowqual_snv_count = lowqual_snv_count + 1
+
+				# Get percentage of low quality SNVs
 				lowqual_percent = (float(lowqual_snv_count)/float(pass_snv_count))*100
 
 				# Close file for buffering
 				bcf_file.close()
+
 				# delete temp vcf file
-				os.system("rm " + bcf + ".vcf")
+				os.system("rm " + pwd + "/" + os.path.basename(bcf) + ".vcf")
+
 				# Output summary data
 				output_snv_summary.write(os.path.basename(bcf) + '\t' + 
 					str(pass_snv_count) + '\t' + str(lowqual_snv_count) + '\t' + str(lowqual_percent) + '\n')
+
+	# Close output file for buffering
 	output_snv_summary.close()
 
 # call main function
