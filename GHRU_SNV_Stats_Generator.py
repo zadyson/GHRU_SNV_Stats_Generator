390aa32eb336356cd41b35fc4284002a1873154c
Zoe Anne Dyson <zadyson@gmail.com>
2021-11-10 15:36:44 +0000

    SNV counts in non-excluded regions

--- GHRU_SNV_Stats_Generator.py
+++ GHRU_SNV_Stats_Generator.py
@@ -16,7 +16,7 @@ from argparse import (ArgumentParser, FileType)
 import os, sys, re, collections, operator
 
 # Version number
-__version__='0.0.3'
+__version__='0.0.4'
 
 # Set up argument parser 
 def parse_args():
@@ -26,6 +26,8 @@ def parse_args():
 		help="Show version number and exit")
 	parser.add_argument('--bcf', nargs='+', type=str, required=True,
 		help='File path to bcf(s) to be analysed')
+	parser.add_argument('--excluded_regions', type=str, required=True,
+		help='Path to tsv file of regions to exclude (i.e. phages and repeats). Should not include a header line.')
 	parser.add_argument('--chromosome_size', type=int, required=True,
 		help='Size of reference chromsome')
 	parser.add_argument('--output_prefix', type=str, required=True,
@@ -41,9 +43,17 @@ def main():
 	# Get location of script
 	pwd = os.getcwd()
 
+	# Build list of SNVs to exclude
+	excluded_regions = open(args.excluded_regions , "r")
+	positions_to_exclude = []
+	for region in excluded_regions:
+		region_coords = region.rstrip().split()
+		region_bases = range(int(region_coords[0]),int(region_coords[1]))
+		positions_to_exclude = positions_to_exclude + [int(region_coords[0])] + region_bases + [int(region_coords[1])]
+
 	# Output summary file with header
 	output_snv_summary = open(args.output_prefix  + "_snv_qc_summary.tsv", "w")
-	output_snv_summary.write("File \t PASS \t LowQual \t Percent_LowQual \n")
+	output_snv_summary.write("File \t PASS \t LowQual \t Percent_LowQual \t Non-excluded_PASS \t Non-excluded_LowQual \t Non-excluded_percent_LowQual \n")
 
 	# Open bcf file
 	for bcf in args.bcf:
@@ -54,6 +64,10 @@ def main():
 				pass_snv_count = 0
 				lowqual_snv_count = 0
 				lowqual_percent = 0
+
+				non_excluded_pass_snv_count = 0
+				non_excluded_lowqual_snv_count = 0
+				non_excluded_lowqual_percent = 0
 
 				# Open created vcf file
 				bcf_file = open(pwd + "/" + os.path.basename(bcf) + ".vcf", 'r')
@@ -72,11 +86,22 @@ def main():
 									# Count PASS and LowQual SNVs
 									if (x[6]=="PASS"):
 										pass_snv_count = pass_snv_count + 1
+										if int(x[1]) not in positions_to_exclude:
+											non_excluded_pass_snv_count = non_excluded_pass_snv_count + 1
 									if (x[6]=="LowQual"):
 										lowqual_snv_count = lowqual_snv_count + 1
+										if int(x[1]) not in positions_to_exclude:
+											non_excluded_lowqual_snv_count = non_excluded_lowqual_snv_count + 1
 
 				# Get percentage of low quality SNVs
-				lowqual_percent = (float(lowqual_snv_count)/float(pass_snv_count))*100
+				if pass_snv_count != 0: 
+					lowqual_percent = (float(lowqual_snv_count)/float(pass_snv_count))*100
+				else: 
+					lowqual_percent = 0
+				if non_excluded_pass_snv_count != 0: 
+					non_excluded_lowqual_percent = (float(non_excluded_lowqual_snv_count)/float(non_excluded_pass_snv_count))*100
+				else: 
+					non_excluded_lowqual_percent = 0
 
 				# Close file for buffering
 				bcf_file.close()
@@ -86,7 +111,9 @@ def main():
 
 				# Output summary data
 				output_snv_summary.write(os.path.basename(bcf) + '\t' + 
-					str(pass_snv_count) + '\t' + str(lowqual_snv_count) + '\t' + str(lowqual_percent) + '\n')
+					str(pass_snv_count) + '\t' + str(lowqual_snv_count) + '\t' + str(lowqual_percent) + '\t' +  
+					str(non_excluded_pass_snv_count) + '\t' + str(non_excluded_lowqual_snv_count) + '\t' +
+					str(non_excluded_lowqual_percent) +'\n')
 
 	# Close output file for buffering
 	output_snv_summary.close()
