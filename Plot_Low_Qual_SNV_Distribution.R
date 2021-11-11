239ca04165b044cd17da7113db6aaf6c7dad546b
Zoe Anne Dyson <zadyson@gmail.com>
2021-11-11 00:40:24 +0000

    Add plotting script for low qual SNV distributions

---
+++ Plot_Low_Qual_SNV_Distribution.R
@@ -0,0 +1,62 @@
+# Plot low qual SNV distributions
+# Dr Zoe A Dyson (zoe.dyson@lshtm.ac.uk)
+# Last updated November 11th 2021
+
+# Import required packages
+library(vcfR)
+library(tidyverse)
+library(ggplot2)
+library(patchwork)
+
+
+# Import and reformat vcf file into table
+vcf <- read.vcfR("PID_0267_B6_S149.filtered.bcf.vcf",
+                 verbose = TRUE )
+vcf2 <- tbl_df(vcf@fix)
+
+vcf3 <- vcf2  %>%
+  filter(as.numeric(POS) < 4809037) %>%
+  filter(ALT %in% c("A","G","T","C")) %>%
+  filter(FILTER == "LowQual") %>%
+  filter(!grepl("INDEL", INFO)) %>%
+  # Extract and create a new column for DP4 from INFO
+  mutate(DP4=sub(".*?DP4=(.*?);.*", "\\1",
+                 INFO)) %>%
+  # Split DP4 across 4 columns
+  separate(DP4, c("DP4_fwd_ref","DP4_rev_ref",
+                  "DP4_fwd_alt","DP4_rev_alt"),
+           sep=",") %>%
+  # Calculate the total proportion of reads mapped
+  # to each SNV
+  mutate(total_DP4=(as.numeric(DP4_fwd_ref)+
+                      as.numeric(DP4_rev_ref)+
+                      as.numeric(DP4_fwd_alt)+
+                      as.numeric(DP4_rev_alt))) %>%
+  # Determine proportion of reads mapped to ref and alt
+  mutate(prop_ref=
+           as.numeric(((as.numeric(DP4_fwd_ref)+
+                          as.numeric(DP4_rev_ref))/
+                         as.numeric(total_DP4))),
+         prop_alt=
+           as.numeric(((as.numeric(DP4_fwd_alt)+
+                          as.numeric(DP4_rev_alt))/
+                         as.numeric(total_DP4)))) %>%
+  arrange(as.numeric(POS)) %>%
+  type_convert()
+
+
+#  Generate plots
+plot_points <- vcf3 %>%
+  ggplot(aes(x=POS, y=prop_ref), colour="black") + 
+  geom_jitter(alpha = 0.1, size=2) + 
+  geom_jitter(aes(x=POS, y=prop_alt), colour="red",alpha = 0.1, size=2) + 
+  scale_x_continuous(limits=c(0,4809037)) + 
+  theme_classic()
+
+plot_pos <- vcf3 %>%
+  ggplot(aes(x=POS), colour="black") + 
+  geom_histogram(bins=200) + 
+  scale_x_continuous(limits=c(0,4809037)) + 
+  theme_classic()
+
+plot_points/plot_pos
